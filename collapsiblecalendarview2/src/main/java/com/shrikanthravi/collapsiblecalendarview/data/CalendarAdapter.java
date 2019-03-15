0f1fc2af4a53d0c180eb6818a94c20d0c4e7464d
Jay Ellos <jay.ellos@viseo.com>
2019-03-15 09:40:20 +0800

    fix calendar adapter crashed - ZoneRulesException

--- collapsiblecalendarview2/src/main/java/com/shrikanthravi/collapsiblecalendarview/data/CalendarAdapter.java
+++ collapsiblecalendarview2/src/main/java/com/shrikanthravi/collapsiblecalendarview/data/CalendarAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jakewharton.threetenabp.AndroidThreeTen;
 import com.shrikanthravi.collapsiblecalendarview.R;
 import com.shrikanthravi.collapsiblecalendarview.widget.UICalendar;
 
@@ -34,7 +35,8 @@ public class CalendarAdapter {
     private List<Event>     mEventList = new ArrayList<>();
 
     public CalendarAdapter(Context context) {
-        this.mCal = LocalDate.now().withDayOfMonth(1);
+        AndroidThreeTen.init(context);
+        this.mCal = LocalDate. now().withDayOfMonth(1);
         mInflater = LayoutInflater.from(context);
 
         refresh();
